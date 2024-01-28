2b73a97f0d69b0765d0021c2f8d612eabcbf7592
mlyu <mlyu@cern.ch>
2024-01-28 22:03:11 +0800

    Add remove csv script

---
+++ rm_csv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Data path
+DATA_PATH="demo"
+EXE_PATH="./CSV2root"
+
+
+for dir in `ls -tr ${DATA_PATH}`
+do
+	dir="${DATA_PATH}/$dir"
+	if [[ "$dir" = "${DATA_PATH}/RootData" ]]; then continue; fi
+	if [[ "$dir" == "${DATA_PATH}/*.log" ]]; then continue; fi
+	if [[ "$dir" == "${DATA_PATH}/*.pssettings" ]]; then continue; fi
+
+	echo "--Removing originial CSV file: $dir"
+	rm -rf $dir
+done
