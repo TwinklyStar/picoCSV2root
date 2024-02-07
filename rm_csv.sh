cc48396d7347ba2f7e3c4b111dad80d789b8dcbe
mlyu <mlyu@cern.ch>
2024-02-07 15:30:50 +0800

    Bug fixes

--- rm_csv.sh
+++ rm_csv.sh
@@ -9,8 +9,8 @@ for dir in `ls -tr ${DATA_PATH}`
 do
 	dir="${DATA_PATH}/$dir"
 	if [[ "$dir" = "${DATA_PATH}/RootData" ]]; then continue; fi
-	if [[ "$dir" == "${DATA_PATH}/*.log" ]]; then continue; fi
-	if [[ "$dir" == "${DATA_PATH}/*.pssettings" ]]; then continue; fi
+	if [[ "$dir" == ${DATA_PATH}/*.log ]]; then continue; fi
+	if [[ "$dir" == ${DATA_PATH}/*.pssettings ]]; then continue; fi
 
 	echo "--Removing originial CSV file: $dir"
 	rm -rf $dir
